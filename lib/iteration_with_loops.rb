7cbfba97a93cebafa7bf5484675210e79bb902e3
Jesse Vaughn <jcv221@gmail.com>
2020-09-17 04:24:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,11 @@
+require pry
+
 def find_min_in_nested_arrays(src)
   new_array =[]
  count = 0
  while count < src.length do 
    inner_count = 0
+   binding.pry
    while inner_count < src[count][inner_count].length do
      if src[count][inner_count]
        
